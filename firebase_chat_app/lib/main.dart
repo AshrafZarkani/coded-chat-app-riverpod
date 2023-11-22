045b998d5ffe234403afc382ef2873ddbc5242dc
ashraf zarkani <ashrafzarkani@gmail.com>
2023-11-22 13:03:49 +0300

    integrated firebase with flutter

--- firebase_chat_app/lib/main.dart
+++ firebase_chat_app/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
